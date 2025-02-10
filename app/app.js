71083a69623b6ad01e73940675ec93713089d593
Ashish Kumar Mahobia <63652300+AshishMahobia@users.noreply.github.com>
2025-02-10 15:36:29 +0530

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello, World! Deployed via Jenkins CI/CD and Created by Prakhar ');
+    res.end('Hello, World! Deployed via Jenkins CI/CD and Created by Ashish  ');
 });
 server.listen(3000, () => {
     console.log('Server is running on port 3000');
